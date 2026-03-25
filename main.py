6f676f683ab7742ea2ddff50cf70dce17f9ff007
Vitor Augusto L. <vitor.augustolucindo@gmail.com>
2026-03-24 21:26:17 -0300

    Adicionado em "main" função exponenciar.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import divisao
 import raiz
 import fatorarf
 import subtrairf
+import exponenciarf
 
 app = FastAPI()
 
@@ -15,3 +16,7 @@ app = FastAPI()
 def root():
     # Redireciona o Usuário assim que ele abrir a URL, direto para a URL '/docs' com todas as funções
     return RedirectResponse(url="/docs")
+
+@app.get("/exponenciar")
+def exponenciar(a: float, b: float):
+    return exponenciarf.exponenciar(a, b)
